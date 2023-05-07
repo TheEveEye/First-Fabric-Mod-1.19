1a0ef75d4a740d6b363b66a219278fe94331c58d
TheEveEye <theeveeye@online.de>
2023-05-07 12:28:57 +0200

    Initial commit

---
+++ src/main/java/net/theeveeye/firstmod/FirstMod.java
@@ -0,0 +1,14 @@
+package net.theeveeye.firstmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FirstMod implements ModInitializer {
+	public static final String MOD_ID = "modid";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/theeveeye/firstmod/FirstModClient.java
@@ -0,0 +1,10 @@
+package net.theeveeye.firstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FirstModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
