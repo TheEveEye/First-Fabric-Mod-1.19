e6a548e59cf241dacc62ae3e9bcce137e62aa883
TheEveEye <theeveeye@online.de>
2023-05-07 14:37:54 +0200

    Added: Citrine Raw Citrine Tanzanite Raw Tanzanite Custom Item Group

--- src/main/java/net/theeveeye/firstmod/FirstMod.java
+++ src/main/java/net/theeveeye/firstmod/FirstMod.java
@@ -1,15 +1,19 @@
 package net.theeveeye.firstmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.theeveeye.firstmod.item.ModItemGroup;
+import net.theeveeye.firstmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 // Very important comment
 public class FirstMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
+	public static final String MOD_ID = "firstmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+		ModItemGroup.registerItemGroups();
+		ModItems.registerModItems();
 	}
 }
