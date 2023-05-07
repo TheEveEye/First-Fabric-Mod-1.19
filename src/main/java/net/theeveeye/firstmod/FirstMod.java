7abf0e96f45593d8a57d825c852964f25e3a2b35
TheEveEye <theeveeye@online.de>
2023-05-07 12:35:17 +0200

    added important comment

--- src/main/java/net/theeveeye/firstmod/FirstMod.java
+++ src/main/java/net/theeveeye/firstmod/FirstMod.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// Very important comment
 public class FirstMod implements ModInitializer {
 	public static final String MOD_ID = "modid";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
